684fab75498a770e22676d7c5eb02a1c9ba605b2
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-10 18:58:02 +0200

    update

--- tests/ScanTest.php
+++ tests/ScanTest.php
@@ -2,11 +2,22 @@
 
 class ScanTest extends \PHPUnit\Framework\TestCase
 {
+    public function testStringScan()
+    {
+        $scan = (new \MicroweberPackages\SecurityScanner\Scanner)
+            ->scanFile(__DIR__ . DIRECTORY_SEPARATOR . 'strange-file2.example');
+
+        $this->assertTrue($scan['error']);
+        $this->assertNotEmpty($scan['warnings']);
+    }
+
     public function testFileScan()
     {
         $scan = (new \MicroweberPackages\SecurityScanner\Scanner)
-            ->scanFile(__DIR__ . DIRECTORY_SEPARATOR . 'strange-file.php');
+            ->scanFile(__DIR__ . DIRECTORY_SEPARATOR . 'strange-file.example');
 
+
+        var_dump($scan);
         $this->assertTrue($scan['error']);
         $this->assertNotEmpty($scan['warnings']);
     }
